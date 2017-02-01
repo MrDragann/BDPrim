f682a3999f6446aed5efc821fa6dfa3b986dbdd1
Dragan <Dragan@DESKTOP-J3FMV5N>
2017-02-01 11:25:24 +0200

    жестко

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -6,7 +6,8 @@ using System.Web.Mvc;
 
 namespace WebApplication4.Controllers
 {
-    public class HomeController : Controller
+    [Authorize]
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
--- WebApplication4/Global.asax.cs
+++ WebApplication4/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WebApplication4.App_Start;
 
 namespace WebApplication4
 {
@@ -16,6 +17,7 @@ namespace WebApplication4
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutofacConfig.ConfigureContainer();
         }
     }
 }
